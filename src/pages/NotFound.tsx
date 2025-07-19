1f4cfbe8bd3dece97cfda7364b9920f2edaf80a6
zLuuuck <lucas.toterol.rodrigues@gmail.com>
2025-07-19 10:55:15 -0300

    react-router-dom + 2 pages

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Error 404: Page not Found</h1>
+    </div>
+  );
+};
