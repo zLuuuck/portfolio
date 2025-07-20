6688d9459c333a21a86b6c4372f6150a21591582
zLuuuck <lucas.toterol.rodrigues@gmail.com>
2025-07-19 22:26:08 -0300

    b

--- src/hooks/useLenis.ts
+++ src/hooks/useLenis.ts
@@ -10,7 +10,7 @@ const useLenis = () => {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.5,
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     });
 
